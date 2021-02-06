c4a0c4da269483c148a8c40b01e47c23b69c5de4
lc6464 <64722907+lc6464@users.noreply.github.com>
2021-02-06 12:17:38 +0800

    由相关脚本更新为相关程序。

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # 升学E网通相关程序，具有各种不同的功能。
-> 有了这些脚本，同学们可开心了！<br/>
+> 有了这些程序，同学们可开心了！<br/>
 > **请不要滥用此仓库中的所有程序！**
 
 ### 使用说明
-- `TampermonkeyScripts/` 文件夹下的 JavaScript 文件 `.js` 请作为油猴脚本使用，可在油猴扩展程序的“实用工具”选项卡中找到 `Install from URL`，将对应的 RAW 的 URL 填入并安装即可。
-- `BrowserScripts/` 文件夹下的 JavaScript 文件 `.js` 请在浏览器的开发人员工具中的控制台执行，不要作为油猴脚本使用。
-- TypeScript 文件 `.ts` 均为开发的源代码，在浏览器中不能正常运行，请运行/在油猴中安装 JavaScript 文件 `.js`。另外，此储存库中的所有 `.ts` 文件均为 `TypeScript` 源代码，而不是 `MPEG2-TS` 视频文件！
-- 油猴扩展程序 `Tampermonkey` 请自行搜索安装方法。
+- `CSharp/`
+  - `.cs` 文件均为 C# 源代码文件
+  - `.exe` 文件均为编译后的可执行二进制文件，请在 Windows 10 x64 环境下运行。
+    - Windows x86 未测试，Linux 可尝试 `mono` ，但不一定成功（未测试）。
+- `Python/`
+  - Windows 10 x64 Python 3.9.0 测试通过
+  - Ubuntu 18.04.5 x64 Python 3.9.0 测试通过
+  
+- `TypeScriptAndJavaScript/`
+  - `TampermonkeyScripts/` 文件夹下的 JavaScript 文件 `.js` 请作为油猴脚本使用，可在油猴扩展程序的“实用工具”选项卡中找到 `Install from URL`，将对应的 RAW 的 URL 填入并安装即可。
+  - `BrowserScripts/` 文件夹下的 JavaScript 文件 `.js` 请在浏览器的开发人员工具中的控制台执行，不要作为油猴脚本使用。
+  - TypeScript 文件 `.ts` 均为开发的源代码，在浏览器中不能正常运行，请运行/在油猴中安装 JavaScript 文件 `.js`。另外，此储存库中的所有 `.ts` 文件均为 `TypeScript` 源代码，而不是 `MPEG2-TS` 视频文件！
+  - 油猴扩展程序 `Tampermonkey` 请自行搜索安装方法。
 
 ### 如何贡献
 - 如果您有好的想法，您可以通过 `Issue` 向我提出。
@@ -15,7 +24,13 @@
 - 如果您有能力解决您或他人遇到的缺陷，您可以通过 `Pull Request` 做出贡献。
 
 ### 编写说明
-- 全部使用 [TypeScript](https://www.typescriptlang.org/zh/ "TypeScript 官网") 编写。
-- TypeScript 编译器版本：`4.1.3`
+- [TypeScript](https://www.typescriptlang.org/zh/ "TypeScript 官网")
+  - 编译器版本：`4.1.3`
+- [Python](https://www.python.org "Python 官网")
+  - 使用 `Python 3.9.0` 验证通过。
+  - 理论上来说支持 `Python 3.6` 及更高版本，Python 3 应该都没问题，但建议使用较新版本。
+- [CSharp](https://dotnet.microsoft.com ".NET 官网")
+  - 基于 .NET Framework 4.8（应该是吧）
+  - 使用 `Visual Studio` 包含的编译器，但没有用 Visual Studio 设定 .NET 版本。
 - 制作者：[LC](https://lcwebsite.cn "LC网站")
-- 声明：本储存库内所有 `TypeScript` or `JavaScript` 文件中均不包含任何恶意代码，可放心使用！若您发现真的有第三方提交的恶意代码的存在，恳请您通过 Issue 或者在[我的网站](https://lcwebsite.cn "LC网站")中查找联系方式报告！
+- 声明：本储存库内所有文本文件中均不包含任何恶意代码，可执行二进制文件均由文本文件使用可信的编译器编译而来，可放心使用！若您发现真的有第三方提交的恶意代码的存在，恳请您通过 Issue 或者在[我的网站](https://lcwebsite.cn "LC网站")中查找联系方式报告！
--- TypeScriptAndJavaScript/TampermonkeyScripts/ewtScript.ts
+++ TypeScriptAndJavaScript/TampermonkeyScripts/ewtScript.ts
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name		升学 E 网通广告跳过
 // @namespace	https://lcwebsite.cn/
-// @version		1.2.8-alpha
-// @description	升学 E 网通广告跳过及视频极速播放
+// @version		1.2.8-alpha.2
+// @description	升学 E 网通广告跳过及视频极速播放。
 // @author		LC
 // @match		http*://web.ewt360.com/site-study/*
 // @icon		https://static.lcwebsite.cn/favicon.svg
@@ -26,6 +26,7 @@
 * 1.2.7-alpha.2：增加功能：自动调整清晰度为“清晰”以降低网络占用、暂停视频答题窗口自动确定。	GitHub #1
 * 1.2.7-alpha.3：优化调整清晰度逻辑。
 * 1.2.8-alpha：增加周看课时长显示功能。（两个注释将在下个版本完善）
+* 1.2.8-alpha.2：完善注释（1.2.8-alpha 及此版本未经验证，可能会不稳定或存在较严重的 bug）。
 */
 
 (function ($, styleText) {
@@ -46,19 +47,19 @@
 			if (nw !== null) { nw.close(); window.close(); }
 			location.href = "about:blank"; window.close(); // 经测试，在 Microsoft Edge 88 下，会打开空页，虽然不会被关闭，但可以减少资源占用
 		}
-		function getVideoTime() { // 获取周看课时长（注释会在下一个版本完善）
-			return fetch('/customerApi/api/studyprod/lessonCenter/getUserTimeRanking', {
-				credentials: 'same-origin'
-			}).then(function (response) {
-				if (response.ok) {
-					return response.json();
-				} else {
-					return new Promise(function (resolve) {
+		function getVideoTime() { // 获取周看课时长
+			return fetch('/customerApi/api/studyprod/lessonCenter/getUserTimeRanking', { // fetch 看课时长 API
+				credentials: 'same-origin' // 发送验证信息 (cookies)
+			}).then(function (response) { // 接收到服务器返回数据后执行
+				if (response.ok) { // 判断是否出现 HTTP 异常
+					return response.json(); // 如果正常，则获取 JSON 数据
+				} else { // 若不正常，返回异常信息
+					return new Promise(function (resolve) { // 这里用 Promise 的原因是 response.json 返回的是 Promise，方便统一处理，下面那个 Promise 也是同理
 						resolve({ success: false, msg: `服务器返回异常 HTTP 状态码：HTTP ${response.statusText}.` });
 					});
 				}
-			}).catch(function (reason) {
-				return new Promise(function (resolve) {
+			}).catch(function (reason) { // 若与服务器连接异常
+				return new Promise(function (resolve) { // 使用 Promise 的原因同上
 					resolve({ success: false, msg: '连接服务器过程中出现异常，消息：' + reason.message });
 				});
 			});
@@ -70,6 +71,7 @@
 			{ // 创建并添加刷新按钮
 				const button: HTMLButtonElement = document.createElement('button');
 				button.innerHTML = '刷新';
+				button.style.width = '8rem';
 				button.addEventListener('click', function () {
 					location.reload();
 				});
@@ -108,23 +110,30 @@
 			}
 			style.innerHTML = styleText; // 给 <style> 赋值
 			document.head.appendChild(style); // 将 <style> 加入 head
-			(async function () { // 增加获取周看课时长显示 <div>（注释将在下个版本完善）
-				const timeDiv: HTMLDivElement = document.createElement('div'),
+			(async function () { // 增加获取周看课时长显示 <div>
+				const timeDiv: HTMLDivElement = document.createElement('div'), // 创建三个元素
 					span: HTMLSpanElement = document.createElement('span'),
 					button: HTMLButtonElement = document.createElement('button');
-				timeDiv.innerText = '周看课时长：';
-				const playTime = await getVideoTime();
-				span.innerText = playTime.success ? (playTime.data.playTime + 'min') : '加载失败';
-				button.innerText = '刷新';
-				button.addEventListener('click', async function () {
-					this.innerText = '刷新中……';
-					const playTime = await getVideoTime();
-					span.innerText = playTime.success ? (playTime.data.playTime + 'min') : '加载失败';
-					button.innerText = '刷新';
+				timeDiv.innerText = '周看课时长：'; // 设置 <div> 内容
+				const playTime = await getVideoTime(); // 获取时长
+				span.innerText = playTime.success ? (playTime.data.playTime + 'min') : '加载失败'; // 将时长写入 <span>
+				button.innerText = '刷新'; // 设置按钮内容
+				button.style.height = '3.5rem';
+				button.style.fontSize = '2rem';
+				button.style.width = '6rem';
+				button.addEventListener('click', async function () { // 按钮按下事件（刷新时长）
+					this.innerText = '刷新中……'; // 更改按钮内容
+					button.style.width = '8rem';
+					button.style.fontSize = '1.5rem';
+					const playTime = await getVideoTime(); // 获取时长
+					span.innerText = playTime.success ? (playTime.data.playTime + 'min') : '加载失败'; // 将时长写入 <span>
+					button.innerText = '刷新'; // 更改按钮内容
+					button.style.fontSize = '2rem';
+					button.style.width = '6rem';
 				});
-				timeDiv.appendChild(span);
-				timeDiv.appendChild(button);
-				div.appendChild(timeDiv);
+				timeDiv.appendChild(span); // 先将时长 <span> 加入 <div>
+				timeDiv.appendChild(button); // 再将刷新按钮加入 <div>
+				div.appendChild(timeDiv); // 将 <div> 添加进控制面板 <div>
 			})();
 			document.body.appendChild(div); // 将 <div> 加入 body
 			{ // 创建并添加显示/隐藏控制面板按钮
@@ -278,13 +287,12 @@
 	user-select: none;
 	transition: top .5s ease-in-out;
 }
-#LC_Tampermonkery_ewtScript_Settings > button:first-of-type {
-	width: 8rem;
-	height: 4rem;
+#LC_Tampermonkery_ewtScript_Settings button {
 	font-size: 2.5rem;
 	background: #444;
 	border: none;
 	border-radius: 1.5rem;
+	height: 4rem;
 }
 #LC_Tampermonkery_ewtScript_loopVideoLabel {
 	display: block;
